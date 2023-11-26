43d0c9be23754fefcda00969a0aef22bc40c2499
MehimoNemo <throwaway113371@gmail.com>
2023-11-26 01:27:29 -0600

    Players could shrink before this commit clientside only. These new lines should implement dynamic mask hud scaling and animated scaling over time. additionally, added infrastructure for other objects to be shrunk.

--- LethalCompanyTemplate/Plugin.cs
+++ LethalCompanyTemplate/Plugin.cs
@@ -1,14 +1,47 @@
 ﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+using GameNetcodeStuff;
+using BepInEx.Configuration;
+using DunGen;
+using UnityEngine.InputSystem;
+using static UnityEngine.ParticleSystem.PlaybackState;
+using LCShrinkRay.comp;
 
-namespace LethalCompanyTemplate
+namespace LCShrinkRay
 {
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
     public class Plugin : BaseUnityPlugin
     {
+        private readonly Harmony harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+        private static Plugin Instance;
+        internal ManualLogSource mls;
+        private GameObject playerObject;
+
         private void Awake()
         {
             // Plugin startup logic
             Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(PluginInfo.PLUGIN_GUID);
+
+            mls.LogInfo("The Test Mod Has Awoken");
+
+            harmony.PatchAll(typeof(Plugin));
+        }
+
+        
+        private void OnDestroy()
+        {
+            GameObject gameObject = new GameObject("SHRINKING");
+            DontDestroyOnLoad(gameObject);
+            gameObject.AddComponent<shrinking>();
+            Logger.LogInfo($"SHRINKING Started!");
         }
     }
 }
---
+++ LethalCompanyTemplate/comp/shrinking.cs
@@ -0,0 +1,101 @@
+﻿using BepInEx.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using GameNetcodeStuff;
+
+
+
+namespace LCShrinkRay.comp
+{
+    internal class shrinking : MonoBehaviour
+    {
+        Transform playerTransform;
+        Transform helmetHudTransform;
+        internal ManualLogSource mls;
+        public void Awake()
+        {
+            mls = BepInEx.Logging.Logger.CreateLogSource(PluginInfo.PLUGIN_GUID);
+            mls.LogInfo("PENIS PENIS PENIS");
+            playerTransform = GameObject.Find("Player").GetComponent<Transform>();
+            helmetHudTransform = GameObject.Find("ScavengerHelmet").GetComponent<Transform>();
+            helmetHudTransform.localPosition = new Vector3(-0.0f, 0.058f, -0.274f);
+        }
+        public void Update()
+        {
+            //mls.LogInfo("\n\n\n\n\n\n HELP \n\n\n\n\n\n");
+            if (Keyboard.current.nKey.wasPressedThisFrame)
+            {
+                mls.LogInfo("Shrinking player models");
+                float scale = 0.2f;
+                PlayerShrinkAnimation(scale, playerTransform, helmetHudTransform);
+            }
+
+        }
+        public void ObjectShrinkAnimation(float shrinkAmt, Transform objectTransform)
+        {
+
+            float shrinkage;
+            float amplitude = 0.5f;
+            DateTime oldTime = DateTime.Now;
+            while (true)
+            {
+                TimeSpan timeSpan = DateTime.Now - oldTime;
+                float time = (float)timeSpan.TotalSeconds;
+                shrinkage = (float)-((Math.Pow(time, 3) - time * amplitude * Math.Sin(time * Math.PI)));
+                if (shrinkage < shrinkAmt)
+                {
+                    objectTransform.localScale = new Vector3(shrinkAmt, shrinkAmt, shrinkAmt);
+                    break;
+                }
+
+            }
+        }
+        public void PlayerShrinkAnimation(float shrinkAmt, Transform playerTransform, Transform maskTransform)
+        {
+
+            float shrinkage;
+            float amplitude = 0.5f;
+            float duration = 3f;
+            DateTime oldTime = DateTime.Now;
+            while (true)
+            {
+                TimeSpan timeSpan = DateTime.Now - oldTime;
+                float time = (float)timeSpan.TotalSeconds/duration;
+                shrinkage = (float)-((Math.Pow(time, 3) - time * amplitude * Math.Sin(time * Math.PI)))+1;
+                if (shrinkage < shrinkAmt)
+                {
+                    playerTransform.localScale = new Vector3(shrinkAmt, shrinkAmt, shrinkAmt);
+                    maskTransform.localScale = CalcMaskScaleVec(shrinkAmt);
+                    maskTransform.localPosition= CalcMaskPosVec(shrinkAmt);
+                    break;
+                }
+                playerTransform.localScale = new Vector3(shrinkage, shrinkage, shrinkage);
+                maskTransform.localScale = CalcMaskScaleVec(shrinkage);
+                maskTransform.localPosition = CalcMaskPosVec(shrinkage);
+
+            }
+        }
+        public Vector3 CalcMaskPosVec(float shrinkScale)
+        {
+            Vector3 pos;
+            float x = 0;
+            float y = 0.00375f*shrinkScale+0.05425f;
+            float z = 0.005f * shrinkScale - 0.279f;
+            pos = new Vector3(x, y, z);
+            return pos;
+        }
+
+        public Vector3 CalcMaskScaleVec(float shrinkScale)
+        {
+            Vector3 pos;
+            float x = 0.277f * shrinkScale + 0.2546f;
+            float y = 0.2645f * shrinkScale + 0.267f;
+            float z = 0.177f * shrinkScale + 0.3546f;
+            pos = new Vector3(x, y, z);
+            return pos;
+        }
+    }
+}
