6ec684d362420a573149bbb04a716a6f7603c1c0
MehimoNemo <throwaway113371@gmail.com>
2023-11-27 23:15:37 -0600

    Added shrink animation and keybinds to change player and player 1 size. Working on implementing grabbable item offset and maybe player size detection.

--- LethalCompanyTemplate/comp/shrinking.cs
+++ LethalCompanyTemplate/comp/shrinking.cs
@@ -5,78 +5,147 @@ using System.Text;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using GameNetcodeStuff;
-
-
+using System.Collections;
+using Unity.Netcode;
+using System.Linq;
 
 namespace LCShrinkRay.comp
 {
     internal class shrinking : MonoBehaviour
     {
         Transform playerTransform;
+        Transform player1Transform;
         Transform helmetHudTransform;
         internal ManualLogSource mls;
+        //GrabbableObject[] grabbables;
         public void Awake()
         {
             mls = BepInEx.Logging.Logger.CreateLogSource(PluginInfo.PLUGIN_GUID);
             mls.LogInfo("PENIS PENIS PENIS");
-            playerTransform = GameObject.Find("Player").GetComponent<Transform>();
+            /*playerTransform = GameObject.Find("Player").GetComponent<Transform>();
             helmetHudTransform = GameObject.Find("ScavengerHelmet").GetComponent<Transform>();
-            helmetHudTransform.localPosition = new Vector3(-0.0f, 0.058f, -0.274f);
+            helmetHudTransform.localPosition = new Vector3(-0.0f, 0.058f, -0.274f);*/ 
         }
         public void Update()
         {
-            //mls.LogInfo("\n\n\n\n\n\n HELP \n\n\n\n\n\n");
-            if (Keyboard.current.nKey.wasPressedThisFrame)
-            {
-                mls.LogInfo("Shrinking player models");
-                float scale = 0.2f;
-                PlayerShrinkAnimation(scale, playerTransform, helmetHudTransform);
-            }
+            //If player picks up something and is short, change the grabbleObject.item.positionOffset by -0.2 0.5 -0.5(still need to test with other objects besides apparatus)
+            //for each grabbable object, if public PlayerControllerB playerHeldBy does not equal null, find out which player and if they're shrunk. If they are, change the item offset.
+            GameObject.FindAnyObjectByType(typeof(GrabbableObject));
+            //grabbables = (GrabbableObject[])FindObjectsOfType(typeof(GrabbableObject));
+            mls.LogMessage(FindObjectsOfType(typeof(GrabbableObject)));
+            GrabbableObject[] grabbables = (GrabbableObject[])FindObjectsOfType<GrabbableObject>().Where(x => x.playerHeldBy != null);
+            mls.LogMessage(grabbables);
 
+
+
+            if (playerTransform == null)
+            {
+                try
+                {
+                    playerTransform = GameObject.Find("Player").GetComponent<Transform>();
+                    helmetHudTransform = GameObject.Find("ScavengerHelmet").GetComponent<Transform>();
+                    helmetHudTransform.localPosition = new Vector3(-0.0f, 0.058f, -0.274f);
+                    mls.LogInfo("Player transform got!");
+                } catch(Exception e) { }
+                try
+                {
+                    player1Transform = GameObject.Find("Player (1)").GetComponent<Transform>();
+                } catch(Exception e) { }
+            }
+            //mls.LogInfo("\n\n\n\n\n\n HELP \n\n\n\n\n\n");
+            try
+            {
+                if (Keyboard.current.nKey.wasPressedThisFrame)
+                {
+                    mls.LogInfo("Shrinking player model");
+                    float scale = 0.2f;
+                    PlayerShrinkAnimation(scale, playerTransform, helmetHudTransform);
+                }
+                if (Keyboard.current.mKey.wasPressedThisFrame)
+                {
+                    mls.LogInfo("Growing player model");
+                    float scale = 1f;
+                    PlayerShrinkAnimation(scale, playerTransform, helmetHudTransform);
+                }
+                if (Keyboard.current.jKey.wasPressedThisFrame)
+                {
+                    mls.LogInfo("Shrinking player(1) model");
+                    float scale = 0.2f;
+                    ObjectShrinkAnimation(scale, player1Transform);
+                }
+                if (Keyboard.current.kKey.wasPressedThisFrame)
+                {
+                    mls.LogInfo("Growing player(1) model");
+                    float scale = 1f;
+                    ObjectShrinkAnimation(scale, player1Transform);
+                }
+            }
+            catch(Exception e) { }
         }
+        
+
+
+        //object shrink animation infrastructure!
         public void ObjectShrinkAnimation(float shrinkAmt, Transform objectTransform)
         {
-
-            float shrinkage;
-            float amplitude = 0.5f;
-            DateTime oldTime = DateTime.Now;
-            while (true)
-            {
-                TimeSpan timeSpan = DateTime.Now - oldTime;
-                float time = (float)timeSpan.TotalSeconds;
-                shrinkage = (float)-((Math.Pow(time, 3) - time * amplitude * Math.Sin(time * Math.PI)));
-                if (shrinkage < shrinkAmt)
-                {
-                    objectTransform.localScale = new Vector3(shrinkAmt, shrinkAmt, shrinkAmt);
-                    break;
-                }
-
-            }
+            StartCoroutine(ObjectShrinkAnimationCoroutine(shrinkAmt, objectTransform));
         }
+
+        private IEnumerator ObjectShrinkAnimationCoroutine(float shrinkAmt, Transform objectTransform)
+        {
+            float duration = 2f;
+            float elapsedTime = 0f;
+            float shrinkage = 1f;
+
+            while (elapsedTime < duration && shrinkage > shrinkAmt)
+            {
+                //shrinkage = -(Mathf.Pow(elapsedTime / duration, 3) - (elapsedTime / duration) * amplitude * Mathf.Sin((elapsedTime / duration) * Mathf.PI)) + 1f;
+                shrinkage = (float)(0.58 * Math.Sin((4 * elapsedTime / duration) + 0.81) + 0.58);
+                //mls.LogFatal(shrinkage);
+                objectTransform.localScale = new Vector3(shrinkage, shrinkage, shrinkage);
+
+                elapsedTime += Time.deltaTime;
+                yield return null; // Wait for the next frame
+            }
+
+            // Ensure final scale is set to the desired value
+            objectTransform.localScale = new Vector3(shrinkAmt, shrinkAmt, shrinkAmt);;
+        }
+
+
+
+
+
+        //Player Shrink animation, shrinks a player over a sinusoidal curve for a duration. Requires the player and mask transforms.
         public void PlayerShrinkAnimation(float shrinkAmt, Transform playerTransform, Transform maskTransform)
         {
+            StartCoroutine(PlayerShrinkAnimationCoroutine(shrinkAmt, playerTransform, maskTransform));
+        }
 
-            float shrinkage;
+        private IEnumerator PlayerShrinkAnimationCoroutine(float shrinkAmt, Transform playerTransform, Transform maskTransform)
+        {
             float amplitude = 0.5f;
-            float duration = 3f;
-            DateTime oldTime = DateTime.Now;
-            while (true)
+            float duration = 2f;
+            float elapsedTime = 0f;
+            float shrinkage = 1f;
+
+            while (elapsedTime < duration && shrinkage>shrinkAmt)
             {
-                TimeSpan timeSpan = DateTime.Now - oldTime;
-                float time = (float)timeSpan.TotalSeconds/duration;
-                shrinkage = (float)-((Math.Pow(time, 3) - time * amplitude * Math.Sin(time * Math.PI)))+1;
-                if (shrinkage < shrinkAmt)
-                {
-                    playerTransform.localScale = new Vector3(shrinkAmt, shrinkAmt, shrinkAmt);
-                    maskTransform.localScale = CalcMaskScaleVec(shrinkAmt);
-                    maskTransform.localPosition= CalcMaskPosVec(shrinkAmt);
-                    break;
-                }
+                //shrinkage = -(Mathf.Pow(elapsedTime / duration, 3) - (elapsedTime / duration) * amplitude * Mathf.Sin((elapsedTime / duration) * Mathf.PI)) + 1f;
+                shrinkage = (float)(0.58 * Math.Sin((4 * elapsedTime / duration)+0.81) + 0.58);
+                //mls.LogFatal(shrinkage);
                 playerTransform.localScale = new Vector3(shrinkage, shrinkage, shrinkage);
                 maskTransform.localScale = CalcMaskScaleVec(shrinkage);
                 maskTransform.localPosition = CalcMaskPosVec(shrinkage);
 
+                elapsedTime += Time.deltaTime;
+                yield return null; // Wait for the next frame
             }
+
+            // Ensure final scale is set to the desired value
+            playerTransform.localScale = new Vector3(shrinkAmt, shrinkAmt, shrinkAmt);
+            maskTransform.localScale = CalcMaskScaleVec(shrinkAmt);
+            maskTransform.localPosition = CalcMaskPosVec(shrinkAmt);
         }
         public Vector3 CalcMaskPosVec(float shrinkScale)
         {
