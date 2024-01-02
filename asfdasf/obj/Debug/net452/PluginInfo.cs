69ce61f770627d6fc132a6142805d5840a84ef70
MehimoNemo <22804739+MehimoNemo@users.noreply.github.com>
2024-01-02 16:45:21 -0600

    Delete asfdasf directory

--- asfdasf/obj/Debug/net452/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace asfdasf
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "asfdasf";
-        public const string PLUGIN_NAME = "asfdasf";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
