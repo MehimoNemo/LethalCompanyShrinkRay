ba541469702bffe990c756407e52161927ddab66
MehimoNemo <22804739+MehimoNemo@users.noreply.github.com>
2024-01-03 14:47:05 -0600

    Delete LethalCompanyTemplate/obj directory

--- LethalCompanyTemplate/obj/Debug/netstandard2.1/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace LCShrinkRay
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "ShrinkRay";
-        public const string PLUGIN_NAME = "ShrinkRay";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
