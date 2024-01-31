d73a52d58fd31e87d7c56b43336fb77dbf3dcba3
Scarlett Pansegrauw <56158418+QueenScarlett23@users.noreply.github.com>
2024-01-31 10:58:34 +0200

    Create PluginInfo.cs

---
+++ LethalCompanyTemplate/PluginInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LCShrinkRay
+{
+    internal class PluginInfo
+    {
+        public const string PLUGIN_GUID = "LCShrinkRay.WIP";
+        public const string PLUGIN_NAME = "LCShrinkRay";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
