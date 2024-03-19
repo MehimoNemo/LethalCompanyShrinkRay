8ada8e284bb0fa349e3ca6b7aa3f50425c85b7af
NiroDev <niros.email@gmail.com>
2024-03-19 12:50:48 +0100

    changed plugin version in advance

--- LittleCompany/PluginInfo.cs
+++ LittleCompany/PluginInfo.cs
@@ -4,6 +4,6 @@
     {
         public const string PLUGIN_GUID = "LittleCompany.BETA";
         public const string PLUGIN_NAME = "LittleCompany";
-        public const string PLUGIN_VERSION = "0.4.0";
+        public const string PLUGIN_VERSION = "0.5.0";
     }
 }
