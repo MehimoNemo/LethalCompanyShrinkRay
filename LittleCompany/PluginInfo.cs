08309fbf88a3b513f3cd668e29d464199d4fa053
NiroDev <niros.email@gmail.com>
2024-06-18 22:09:12 +0200

    version increase

--- LittleCompany/PluginInfo.cs
+++ LittleCompany/PluginInfo.cs
@@ -4,6 +4,6 @@
     {
         public const string PLUGIN_GUID = "Toybox.LittleCompany";
         public const string PLUGIN_NAME = "LittleCompany";
-        public const string PLUGIN_VERSION = "1.2.0";
+        public const string PLUGIN_VERSION = "1.2.1";
     }
 }
