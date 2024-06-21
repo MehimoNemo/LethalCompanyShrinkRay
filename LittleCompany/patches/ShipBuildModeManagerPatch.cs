e628c2cc1a050065e4d50699da4b6836b1092f28
Samuel <samuda31@hotmail.com>
2024-06-21 11:55:05 -0400

    - Fix shipbuilding moving

--- LittleCompany/patches/ShipBuildModeManagerPatch.cs
+++ LittleCompany/patches/ShipBuildModeManagerPatch.cs
@@ -18,35 +18,20 @@ namespace LittleCompany.patches
             if (__instance.placingObject != null)
             {
                 ShipObjectScaling scaling = ShipObjectModification.ScalingOf(__instance.placingObject);
-                float relativeScaleOfParent = scaling.RelativeScale;
-                Plugin.Log("Scale: " + relativeScaleOfParent);
-                Vector3 scale = Vector3.Scale(__instance.placingObject.mainMesh.transform.localScale, __instance.placingObject.parentObject.transform.localScale) / relativeScaleOfParent;
+                Vector3 scale = Vector3.Scale(__instance.placingObject.mainMesh.transform.localScale, __instance.placingObject.parentObject.transform.localScale) / scaling.RelativeScale;
                 __instance.ghostObjectMesh.transform.localScale = scale;
-                __instance.selectionOutlineMesh.transform.localScale = scale;
-                __instance.ghostObjectMesh.transform.position -= Vector3.up * (scaling.offsetPivotToBottom * relativeScaleOfParent);
+                __instance.selectionOutlineMesh.transform.localScale = scale * 1.04f;
             }
         }
 
-        [HarmonyPatch(typeof(ShipBuildModeManager), "EnterBuildMode")]
-        [HarmonyPrefix]
-        public static void EnterBuildMode_Prefix(InputAction.CallbackContext context, ShipBuildModeManager __instance)
+        [HarmonyPatch(typeof(ShipBuildModeManager), "Update")]
+        [HarmonyPostfix]
+        public static void Update_PostFix(ShipBuildModeManager __instance)
         {
-            if (!context.performed || GameNetworkManager.Instance == null || GameNetworkManager.Instance.localPlayerController == null || GameNetworkManager.Instance.localPlayerController.isTypingChat)
-            {
-                return;
-            }
             if (__instance.InBuildMode)
             {
-                if (!(__instance.timeSincePlacingObject <= 1f) && __instance.PlayerMeetsConditionsToBuild())
-                {
-                    if (!__instance.CanConfirmPosition)
-                    {
-                        return;
-                    }
-                    ShipObjectScaling scaling = ShipObjectModification.ScalingOf(__instance.placingObject);
-                    __instance.ghostObject.position -= Vector3.up * (scaling.offsetPivotToBottom * scaling.RelativeScale);
-                }
-                return;
+                ShipObjectScaling scaling = ShipObjectModification.ScalingOf(__instance.placingObject);
+                __instance.ghostObject.position -= Vector3.up * (__instance.placingObject.yOffset - (__instance.placingObject.yOffset * scaling.RelativeScale));
             }
         }
     }
