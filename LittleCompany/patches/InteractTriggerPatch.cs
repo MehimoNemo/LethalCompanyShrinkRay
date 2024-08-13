aa02d84b0af11c470067d246629ba407f10b8f6e
Samuel <samuda31@hotmail.com>
2024-08-13 16:32:52 -0400

    - cleaning up logs

--- LittleCompany/patches/InteractTriggerPatch.cs
+++ LittleCompany/patches/InteractTriggerPatch.cs
@@ -16,11 +16,9 @@ namespace LittleCompany.patches
         [HarmonyPrefix]
         public static bool InteractPrefix(Transform playerTransform, InteractTrigger __instance)
         {
-            Plugin.Log("InteractPrefix");
             if (!ModConfig.Instance.values.resizeWhenInVehicle && (__instance.name == "DriverSeatTrigger" || __instance.name == "PassengerSeatTrigger"))
             {
                 PlayerControllerB playerControllerB = playerTransform.GetComponent<PlayerControllerB>();
-                Plugin.Log("playerControllerB");
                 if (playerControllerB == PlayerInfo.CurrentPlayer)
                 {
                     float playerScale = 1f;
@@ -30,17 +28,13 @@ namespace LittleCompany.patches
                     if (playerScaling != null) playerScale = playerScaling.RelativeScale;
                     VehicleScaling vehicleScaling = __instance.GetComponentInParent<VehicleScaling>();
                     if (vehicleScaling != null) vehicleScale = vehicleScaling.RelativeScale;
-                    Plugin.Log("playerScale " + playerScale);
-                    Plugin.Log("vehicleScale " + vehicleScale);
                     if (playerScale > vehicleScale)
                     {
                         HUDManager.Instance.DisplayTip("Company Cruiser", "You are too big to fit the cruiser.", false, false, "littleCompany-Hint");
-                        Plugin.Log("return false");
                         return false;
                     }
                 }
             }
-            Plugin.Log("return true");
             return true;
         }
     }
